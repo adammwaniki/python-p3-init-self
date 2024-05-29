144819519dd4ac60d7f9f0623f2f86bf168a9375
Adam Mwaniki <adamndegwa@protonmail.com>
2024-05-30 02:04:23 +0300

    Completes lab with test scenarios

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+mbwa_kali = Dog("Simba")
+print(mbwa_kali.name)
+
+mbwa_kali.name = "Rex"
+print(mbwa_kali.name)
+
+mbwa_kali.breed = "German Shepherd"
+print(mbwa_kali.breed)
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+jambazi_sugu = Person("John Kiriamiti")
+print(jambazi_sugu.name)
+
